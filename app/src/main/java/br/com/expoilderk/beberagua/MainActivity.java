ab0ad3e95c3f0cfc596571e10c0064613ebbfc89
expoilderk <falecommarlonreis@gmail.com>
2021-07-07 21:30:08 -0300

    Grava os dados de prefencias do usuario

--- app/src/main/java/br/com/expoilderk/beberagua/MainActivity.java
+++ app/src/main/java/br/com/expoilderk/beberagua/MainActivity.java
@@ -1,13 +1,17 @@
 package br.com.expoilderk.beberagua;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,34 +23,78 @@ public class MainActivity extends AppCompatActivity {
     private int minute;
     private int interval;
 
+    private boolean activated = false;
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         btnNotify = findViewById(R.id.btn_notify);
         editMinutes = findViewById(R.id.edit_txt_number_interval);
         timePicker = findViewById(R.id.time_picker);
 
         timePicker.setIs24HourView(true);
+        preferences = getSharedPreferences("db", Context.MODE_PRIVATE);
 
-        btnNotify.setOnClickListener(notifyClick);
+        activated = preferences.getBoolean("activated", false);
+        if(activated){
+            btnNotify.setText(R.string.pause);
+            btnNotify.setBackgroundTintList(ContextCompat.getColorStateList(this, android.R.color.black));
 
+            int interval = preferences.getInt("interval", 0);
+            int hour = preferences.getInt("hour", timePicker.getCurrentHour());
+            int minute = preferences.getInt("minute", timePicker.getCurrentMinute());
+
+            editMinutes.setText(String.valueOf(interval));
+            timePicker.setCurrentHour(hour);
+            timePicker.setCurrentMinute(minute);
+        }
 
     }
 
-    public View.OnClickListener notifyClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            String sInterval = editMinutes.getText().toString();
+    public void notifyClick(View view) {
+        String sInterval = editMinutes.getText().toString();
 
-            hour = timePicker.getCurrentHour();
-            minute = timePicker.getCurrentMinute();
-            interval = Integer.parseInt(sInterval);
-
-            Log.d("Teste", "hora: " + hour + " minuto: " + minute + " intervalo: " + interval);
+        if(sInterval.isEmpty()){
+             Toast.makeText(this, R.string.error_msg, Toast.LENGTH_LONG).show();
+             return;
         }
-    };
 
+        hour = timePicker.getCurrentHour();
+        minute = timePicker.getCurrentMinute();
+        interval = Integer.parseInt(sInterval);
+
+        if(!activated){
+            btnNotify.setText(R.string.pause);
+            btnNotify.setBackgroundTintList(ContextCompat.getColorStateList(this, android.R.color.black));
+            activated = true;
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("activated", true);
+            editor.putInt("interval", interval);
+            editor.putInt("hour", hour);
+            editor.putInt("minute", minute);
+            editor.apply();
+
+        } else {
+            btnNotify.setText(R.string.notify);
+            btnNotify.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.button));
+            activated = false;
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("activated", false);
+            editor.remove("interval");
+            editor.remove("hour");
+            editor.remove("minute");
+            editor.apply();
+        }
+
+
+
+
+
+        Log.d("Teste", "hora: " + hour + " minuto: " + minute + " intervalo: " + interval);
+    }
 }
